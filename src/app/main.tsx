a99f7b9c05317255ab7ed2a27e5272fbc48cc58a
OneMight <dimamirankov63@gmail.com>
2026-01-05 15:39:43 +0300

    feat: add provider for chat

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,17 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "@/app/styles/index.css";
-import { ProviderRouter, QueryProvider } from "@/app/providers/";
+import {
+  ProviderRouter,
+  QueryProvider,
+  ProviderApollo,
+} from "@/app/providers/";
 import { Suspense } from "react";
 import { UserProvider } from "./providers/UserProvider";
 import { Spinner } from "@/components";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Suspense fallback={<Spinner className="size-10" />}>
       <QueryProvider>
-        <UserProvider>
-          <ProviderRouter />
-        </UserProvider>
+        <ProviderApollo>
+          <UserProvider>
+            <ProviderRouter />
+          </UserProvider>
+        </ProviderApollo>
       </QueryProvider>
     </Suspense>
   </StrictMode>,
